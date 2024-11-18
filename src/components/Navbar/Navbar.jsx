17c1f4c9775ae23ba39a5e359bb43ff9766a47ef
khoa <user.iamjasper@gmail.com>
2024-11-19 01:45:39 +0700

    add login and register

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from '../../assets/assets'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState('home')
 
     return (
@@ -45,7 +45,7 @@ const Navbar = () => {
                     <img src={assets.basket_icon} alt="" />
                     <div className="dot"></div>
                 </div>
-                <button>sign in</button>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
         </div>
     )
