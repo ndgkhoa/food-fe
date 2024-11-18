f0fff59735e3cd8140a00911082a2f8c9e726e89
khoa <user.iamjasper@gmail.com>
2024-11-19 05:50:19 +0700

    add order page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,14 @@
 import { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
     const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
         useContext(StoreContext)
+
+    const navigate = useNavigate()
+
     return (
         <div className="cart">
             <div className="cart-items">
@@ -42,6 +46,15 @@ const Cart = () => {
                 })}
             </div>
             <div className="cart-bottom">
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, Enter it here</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" placeholder="promo code" />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="cart-total">
                     <h2>Cart Totals</h2>
                     <div>
@@ -52,24 +65,22 @@ const Cart = () => {
                         <hr />
                         <div className="cart-total-details">
                             <p>Delivery Fee</p>
-                            <p>${2}</p>
+                            <p>${getTotalCartAmount() > 0 ? 2 : 0}</p>
                         </div>
                         <hr />
                         <div className="cart-total-details">
                             <b>Total</b>
-                            <b>${getTotalCartAmount() + 2}</b>
+                            <b>
+                                $
+                                {getTotalCartAmount() > 0
+                                    ? getTotalCartAmount() + 2
+                                    : 0}
+                            </b>
                         </div>
                     </div>
-                    <button>PROCEED TO CHECKOUT</button>
-                </div>
-                <div className="cart-promocode">
-                    <div>
-                        <p>If you have a promo code, Enter it here</p>
-                        <div className="cart-promocode-input">
-                            <input type="text" placeholder="promo code" />
-                            <button>Submit</button>
-                        </div>
-                    </div>
+                    <button onClick={() => navigate('/order')}>
+                        PROCEED TO CHECKOUT
+                    </button>
                 </div>
             </div>
         </div>
