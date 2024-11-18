a0896119320a8b5f3cea61ee774df81bdf8dec87
khoa <user.iamjasper@gmail.com>
2024-11-18 20:02:22 +0700

    add home page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className="footer" id="footer">
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        In volutpat iaculis placerat. Maecenas interdum, mi eu
+                        laoreet semper, odio orci sodales lectus, vitae
+                        porttitor risus turpis non orci. Proin semper erat et
+                        nisl mattis, vel vestibulum velit sollicitudin.
+                    </p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+0123-456-789</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">
+                Copyright 2024 © Tomato - All Right Reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
